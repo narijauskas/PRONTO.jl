d6a6e4654ec5dec11ab8be79e4d121627f081044
zoeturin <zoeturin@gmail.com>
2021-12-10 19:04:44 -0700

    added typical cost functionals

--- src/cost.jl
+++ src/cost.jl
@@ -2,13 +2,27 @@ using QuadGK
 
 wnorm(vec, mat) = 1/2 * vec'*mat*vec
 
-function LQ_cost(ξ, ξd, Q, R, m, T)
+# hard-code typical cost function and derivatives
+function LQ_cost(ξ, ξd, Q, R, P1, T)
     err = ξ - ξd
-    l = (t) -> ( wnorm(err.x(t), Q(t)) + wnorm(err.u(t), R(t)) )
+    l = t -> wnorm(err.x(t), Q(t)) + wnorm(err.u(t), R(t))
     L, _ = quadgk(l, 0, T)
-    return L + m(ξ.x(T)) #TODO fix m?
+    L += wnorm(err.x(T), P1) # terminal cost
+    return L
 end
 
-function LQ_cost_dt() end #TODO: add time derivative of LQ_cost
+function LQ_cost_dt(ξ, ξd, Q, R, t)
+    # add time derivative of LQ_cost
+    err = ξ - ξd
+    return wnorm(err.x(t), Q(t)) + wnorm(err.u(t), R(t))
+end 
 
-function DLQ_cost() end #TODO: add total derivative of LQ_cost
+function DLQ_cost(ζ, ξ, ξd, Q, R, P1, T) 
+    # add total derivative of LQ_cost
+    err = ξ - ξd
+    l = t -> err.x(t)' * Q * ζ.x + err.u(t)' * R * ζ.u
+    L, _ = quadgk(l, 0, T)
+    L += err.x(T)' * P1 * ζ.x(T) # terminal cost
+    return L
+end
+
