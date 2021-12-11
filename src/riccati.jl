17e6abc3da0d960a525caf9ec3dd48695d65f560
mantas <man61@case.edu>
2021-12-10 19:03:43 -0700

    optKr cleanup

--- src/riccati.jl
+++ src/riccati.jl
@@ -1,10 +1,4 @@
 
-function solve_riccati(A, B, Q, R, T)
-    P₁,_ = arec(A(T), B(T)inv(R)B(T)', Q)
-    solve(ODEProblem(riccati!, P₁, (T,0), (A,B,Q,R)))
-end
-
-
 function riccati!(Ṗ, P, (A,B,Q,R), t)
     Ṗ = -A(t)'P - P*A(t) + P*B(t)*inv(R)*B(t)'P - Q
 end
@@ -12,6 +6,7 @@ end
 
 function optKr(A, B, Q, R, T)
     # create optimal LQ regulator Kr that stabilizes around trajectory  
-    P = solve_riccati(A, B, Q, R, T)# call riccati solver
+    P₁,_ = arec(A(T), B(T)inv(R)B(T)', Q) # solve algebraic riccati eq at time T
+    solve(ODEProblem(riccati!, P₁, (T,0), (A,B,Q,R))) # solve differential riccati
     return Kᵣ = inv(R)*B'*P
 end
