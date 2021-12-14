50c1f36e64f33a7c8737c015bf34d7914b9ff72d
mantas <man61@case.edu>
2021-12-14 03:42:59 -0700

    pendulum

--- scripts/inv_pend.jl
+++ scripts/inv_pend.jl
@@ -1,9 +1,38 @@
 using PRONTO
+using ForwardDiff
+using ForwardDiff: derivative
+using DifferentialEquations
+using MatrixEquations
 using LinearAlgebra
 using GLMakie
 
 ## ------------------------------ USER INPUTS ------------------------------ ## 
-# desired trajectory
+
+
+# ---------------- time ---------------- #
+T = 10 # final time
+# unlike the og version, dt and T0 don't really matter beyond plotting
+dt = 0.01
+T0 = 0:dt:T
+
+# ---------------- desired trajectory ---------------- #
+ϕ₀ = t -> tanh(t-5)*(1-tanh(t-5)^2)
+amp = (π/2)*(1/maximum(ϕ₀.(T0)))
+
+ϕ = t -> amp*tanh(t-5)*(1-tanh(t-5)^2)
+dϕ = t -> derivative(ϕ, t)
+ddϕ = t -> derivative(dϕ, t)
+# sanity check:
+# dϕ = t -> amp*(3tanh(t-5)^4 - 4tanh(t-5)^2 + 1)
+# lines(T0, dϕ.(T0))
+
+g = 9.8; l = 0.5;
+u = t -> [(g*sin(ϕ(t)) - l*ddϕ(t)) / cos(ϕ(t))]
+
+x = t->[ϕ(t),dϕ(t)]
+ξd = Trajectory(x,u)
+
+
 
 
 # regulator parameters
@@ -15,44 +44,30 @@ Qc = I(2)
 Rc = I(1)
 
 # define dynamics
-function fxn(x, u)
+function ipend(x, u)
     # parameters:
-    l = 1; g = 9.8;
+    g = 9.8; l = 0.5;
     # dynamics:
-    [x[2], (g/l)*sin(x[1]) - (u/l) * cos(x[1])]
+    [x[2], (g/l)*sin(x[1]) - (u[1]/l) * cos(x[1])]
 end
 
-T = 10 # final time
 
-A = Jx(f, ξd)
-B = Ju(f, ξd)
+
+# linearize around desired trajectory
+A = Jx(ipend, ξd)
+B = Ju(ipend, ξd)
+
 
 # create cost functional
-P₁,_ = arec(A(T), B(T)inv(Rᵣ)B(T)', Qc) # solve algebraic riccati eq at time T
+P₁,_ = arec(A(T), B(T)inv(Rc)B(T)', Qc) # solve algebraic riccati eq at time T
 l, m = build_LQ_cost(ξd, Qc, Rc, P₁, T) # cost functional
 
 
 Pr₁,_ = arec(A(T), B(T)inv(Rr)B(T)', Qr) # solve algebraic riccati eq at time T
 
 
-# ---------------- from newt_invpend ---------------- #
-dt = 0.01
-T0 = 0:dt:10
 
-ϕ₀ = t -> tanh(t-5)*(1-tanh(t-5)^2)
-# lines(T0, ϕ₀.(T0))
-amp = (π/2)*(1/maximum(ϕ₀.(T0)))
 
-ϕ = t -> amp*tanh(t-5)*(1-tanh(t-5)^2)
-dϕ = t -> derivative(ϕ, t)
-ddϕ = t -> derivative(dϕ, t)
-# sanity check:
-# dϕ = t -> amp*(3tanh(t-5)^4 - 4tanh(t-5)^2 + 1)
-# lines(T0, dϕ.(T0))
-
-GG = 9.81;  LL = 0.5;
-u = t -> (GG*sin(ϕ(t)) - LL*ddϕ(t))/cos(ϕ(t))
-# u = t -> 0.0
 
 
 ## ------------------------------ DO PRONTO STUFF ------------------------------ ## 
