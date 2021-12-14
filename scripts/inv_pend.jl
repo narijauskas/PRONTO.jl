bd20c8e8f9a8becf47779135cbd81452372c3aff
mantas <man61@case.edu>
2021-12-14 03:02:32 -0700

    inv pend

--- scripts/inv_pend.jl
+++ scripts/inv_pend.jl
@@ -22,29 +22,21 @@ P₁,_ = arec(A(T), B(T)inv(Rᵣ)B(T)', Qᵣ) # solve algebraic riccati eq at ti
 # ---------------- from newt_invpend ---------------- #
 dt = 0.01
 T0 = 0:dt:10
-tanhT2 = @. tanh(T0-5)
-lines(T0, tanhT2)
 
-ϕ₀ = t->tanh(t-5)*(1-tanh(t-5)^2)
+ϕ₀ = t -> tanh(t-5)*(1-tanh(t-5)^2)
+# lines(T0, ϕ₀.(T0))
+amp = (π/2)*(1/maximum(ϕ₀.(T0)))
 
-lines(T0, ϕ₀.(T0))
+ϕ = t -> amp*tanh(t-5)*(1-tanh(t-5)^2)
+dϕ = t -> derivative(ϕ, t)
+ddϕ = t -> derivative(dϕ, t)
+# sanity check:
+# dϕ = t -> amp*(3tanh(t-5)^4 - 4tanh(t-5)^2 + 1)
+# lines(T0, dϕ.(T0))
 
-amp = (π/2)/maximum(ϕ₀.(T0))
-
-amp = ( 90 *pi/180 )/Phi_0_max
-
-Phi_des   = amp*(tanhT2.*(1-tanhT2.^2))/Phi_0_max;
-dPhi_des  = amp*(3*tanhT2.^4 - 4*tanhT2.^2 + 1)/Phi_0_max;
-ddPhi_des = amp*(-4*tanhT2.*(3*tanhT2.^4 - 5*tanhT2.^2 + 2))/Phi_0_max;
 GG = 9.81;  LL = 0.5;
-
-
-if  max( abs(Phi_des) ) < 60*pi/180   # be somewhat conservative!
-    Udes = ( GG*sin(Phi_des) - LL*ddPhi_des ) ./ cos(Phi_des);
-else
-    Udes = 0*T0;  # or whatever you might *invent* (??)
-end
-
+u = t -> (GG*sin(ϕ(t)) - LL*ddϕ(t))/cos(ϕ(t))
+# u = t -> 0.0
 
 
 # desired trajectory
