35edce3cfdb9830ce192685a12fdf7b22ce01057
zoeturin <zoeturin@gmail.com>
2021-12-18 21:56:34 -0700

    aaaaa

--- scripts/n_inv_pend.jl
+++ scripts/n_inv_pend.jl
@@ -149,7 +149,6 @@ lines!(ax2, time, KE, color = :red)
 lines!(ax2, time, PE, color = :blue)
 lines!(ax2, time, totE, color = :purple)
 
-dx = zeros(2*N)
 fig
 record(fig, "Npend.mp4", 2:numt, framerate = fps) do frame
     t = tvec[frame]
