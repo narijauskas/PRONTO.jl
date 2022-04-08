800e3abe96eb349b5ddbc35106cc9b67840f29c1
mantas <mantas@case.edu>
2022-04-07 22:43:54 -0600

    api, cleanup

---
+++ dev/api.jl
@@ -0,0 +1,27 @@
+# sketch of high-level PRONTO api
+
+# create t vector, pre-allocate x(t) & u(t) (defines output dimensions)
+# each iteration modifies the trajectory in-place
+pronto!(x, u, t, fxns...; opts...)
+pronto!(ξ, f, l, p; opts...)
+
+# trajectory type
+ξ.x
+ξ.u
+ξ.t
+# maybe: built in projection?
+
+
+# maybe: debug/diagnostic version -> saves each iteration step
+# pronto(x, u, t, fxns...; opts...)
+
+# user provided functions:
+f(x,u,t)
+l(x,u,t)
+p(x)
+
+
+# helper/convenience functions:
+project()
+# ways to make f,l,p
+ξ = ξ_init(x_eq, u_guess, t)
