57fc4d691772318722f340be1ab25d71cd9ac481
mantas <man61@case.edu>
2022-04-21 23:32:54 -0600

    build model script

--- test/inv_pend2.jl
+++ test/inv_pend2.jl
@@ -11,6 +11,24 @@ using GLMakie; display(lines(rand(10)))
 using PRONTO
 using PRONTO: jacobian, hessian
 
+## --------------------------- helper plot trajectories/timeseries --------------------------- ##
+
+function plot_timeseries!(ax, t, X, n; kw...)
+    for i in 1:n
+        lines!(ax, t, map(τ->X(τ)[i], t); kw...)
+    end
+    return ax
+end
+
+function plot_timeseries(t, X, n; kw...)
+    fig = Figure(; kw...)
+    ax = Axis(fig[1,1]; kw...)
+
+    plot_timeseries!(ax, t, X, n; kw...)
+    return fig
+end
+
+
 
 ## --------------------------- problem definition --------------------------- ##
 
@@ -54,66 +72,8 @@ model = merge(model, (
 ));
 
 
-## --------------------------- autodiff --------------------------- ##
-
-model = merge(model, (
-    fx = jacobian(x, model.f, x, u),
-    fu = jacobian(u, model.f, x, u),
-))
-model = merge(model, (
-    fxx = jacobian(x, model.fx, x, u),
-    fxu = jacobian(u, model.fx, x, u),
-    fuu = jacobian(u, model.fu, x, u),
-))
-model = merge(model, (
-    lx = jacobian(x, model.l, x, u),
-    lu = jacobian(u, model.l, x, u),
-))
-model = merge(model, (
-    lxx = jacobian(x, model.lx, x, u),
-    lxu = jacobian(u, model.lx, x, u),
-    luu = jacobian(u, model.lu, x, u),
-))
-
-## --------------------------- terminal cost --------------------------- ##
-
-xt = [0;0]
-ut = [0]
-
-A = model.fx(xt, ut)
-B = model.fu(xt, ut)
-Q = model.lxx(xt, ut)
-R = model.luu(xt, ut)
-S = model.lxu(xt, ut)
-Po,_ = arec(A, B, R, Q ,S)
-p = x -> 1/2*collect(x)'*Po*collect(x)
-
-model = merge(model, (p=p,))
-model = merge(model, (px = jacobian(x, model.p, x),))
-model = merge(model, (pxx = jacobian(x, model.px, x),))
-
-
-
-
-
-
-
-## --------------------------- helper plot trajectories/timeseries --------------------------- ##
-
-function plot_timeseries!(ax, t, X, n; kw...)
-    for i in 1:n
-        lines!(ax, t, map(τ->X(τ)[i], t); kw...)
-    end
-    return ax
-end
-
-function plot_timeseries(t, X, n; kw...)
-    fig = Figure(; kw...)
-    ax = Axis(fig[1,1]; kw...)
-
-    plot_timeseries!(ax, t, X, n; kw...)
-    return fig
-end
+## --------------------------- autodiff & terminal cost --------------------------- ##
+include("build_model.jl")
 
 
 ## --------------------------- zero initial trajectory --------------------------- ##
@@ -133,7 +93,7 @@ x = Timeseries(t->[0;0;], model.t)
 # ξ = (x,u)
 
 ## --------------------------- optimize --------------------------- ##
-@time ξ = pronto(ξ, model)
+ξ = pronto(ξ, model)
 
 
 ## --------------------------- plot --------------------------- ##
