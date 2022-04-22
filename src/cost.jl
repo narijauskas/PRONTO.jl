bb0b6fb871fbcbdbe48fadef4531b85c1f51242f
mantas <man61@case.edu>
2022-04-21 20:30:08 -0600

    cost

--- src/cost.jl
+++ src/cost.jl
@@ -3,9 +3,9 @@ function stage_cost!(dh, h, (l,x,u), t)
     dh .= l(x(t), u(t))
 end
 
-function cost(x,u,t,l)
-    T = last(t)
-    h = solve(ODEProblem(stage_cost!, [0], (0.0,T), (l,x,u)))
+function cost(x,u,model)
+    T = last(model.t)
+    h = solve(ODEProblem(stage_cost!, [0], (0.0,T), (model.l,x,u)))
     return h
 end
  
