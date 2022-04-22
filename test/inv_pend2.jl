1a48c542249ccd630888fe3e00f57017506bb7f3
mantas <man61@case.edu>
2022-04-21 23:30:10 -0600

    tested, working

--- test/inv_pend2.jl
+++ test/inv_pend2.jl
@@ -7,7 +7,7 @@ using LinearAlgebra
 # using DifferentialEquations
 using MatrixEquations
 
-using GLMakie; plot(rand(10))
+using GLMakie; display(lines(rand(10)))
 using PRONTO
 using PRONTO: jacobian, hessian
 
@@ -38,7 +38,7 @@ model = (
     x_eq = [0; 0],
     f = f,
     l = l,
-    maxiters = 10,
+    maxiters = 100,
     tol = 1e-2,
     β = 0.7,
     α = 0.4,
@@ -100,18 +100,18 @@ model = merge(model, (pxx = jacobian(x, model.px, x),))
 
 ## --------------------------- helper plot trajectories/timeseries --------------------------- ##
 
-function plot_trajectory!(ax, t, X, n; kw...)
+function plot_timeseries!(ax, t, X, n; kw...)
     for i in 1:n
         lines!(ax, t, map(τ->X(τ)[i], t); kw...)
     end
     return ax
 end
 
-function plot_trajectory(t, X, n; kw...)
+function plot_timeseries(t, X, n; kw...)
     fig = Figure(; kw...)
     ax = Axis(fig[1,1]; kw...)
 
-    plot_trajectory!(ax, t, X, n; kw...)
+    plot_timeseries!(ax, t, X, n; kw...)
     return fig
 end
 
@@ -133,228 +133,8 @@ x = Timeseries(t->[0;0;], model.t)
 # ξ = (x,u)
 
 ## --------------------------- optimize --------------------------- ##
-ξ = pronto(ξ, model)
-
-
-
-
-
-## --------------------------- regulator --------------------------- ##
-
-# set x_eq, u_eq
-X.u[:,end] .= x_eq = [0.0,0.0]
-
-# % LQR stage costs
-# Pr(x)
-# Qr(x,u,t)
-# Rr(x,u,t)
-Qr = t->diagm([10,1]) # needs to capture X(t)
-Rr = t->1e-3 # needs to capture X(t)
-
-
-Kr,Pt = regulator(X, U, t, Rr, Qr, fx, fu);
-display(plot_trajectory(t, Kr, 2))
-
-# % Regulator equilibrium
-# reg.xr = @(xT) 0*xT;
-# reg.ur = @(xT) [0 0]*xT;
-
-
-# anonymous = 2.4ms to build, 1μs to execute
-# regular function = 2.4ms to build, 1μs to execute
-##
-
-
-
-
-## --------------------------- projection --------------------------- ##
-
-X1,U1 = projection(X, U, t, Kr, x0, f);
-
-fig = Figure()
-ax = Axis(fig[1,1]; title = "X1"); plot_trajectory!(ax, t, X1, 2)
-ax = Axis(fig[2,1]; title = "U1"); plot_trajectory!(ax, t, U1, 1)
-display(fig)
-
-# L = cost(X1,U1,t,l)
-
-
-
-## --------------------------- tangent space --------------------------- ##
-
-
-A = t->Main.fx(X1(t), U1(t))
-B = t->Main.fu(X1(t), U1(t))
-a = t->Main.l_x(X1(t), U1(t))
-b = t->Main.l_u(X1(t), U1(t))
-
-fig = Figure()
-ax = Axis(fig[1,1]; title = "A"); plot_trajectory!(ax, t, A, 2)
-ax = Axis(fig[1,2]; title = "B"); plot_trajectory!(ax, t, B, 1)
-ax = Axis(fig[2,1]; title = "a"); plot_trajectory!(ax, t, a, 1)
-ax = Axis(fig[2,2]; title = "b"); plot_trajectory!(ax, t, b, 1)
-display(fig)
-
-## --------------------------- tangent space cont'd --------------------------- ##
-
-
-Q = t->Main.lxx(X1(t), U1(t))
-R = t->Main.luu(X1(t), U1(t))
-S = t->Main.lxu(X1(t), U1(t))
-
-fig = Figure()
-ax = Axis(fig[1,1]; title = "Q"); plot_trajectory!(ax, t, Q, 2)
-ax = Axis(fig[2,1]; title = "R"); plot_trajectory!(ax, t, R, 1)
-ax = Axis(fig[3,1]; title = "S"); plot_trajectory!(ax, t, S, 1)
-display(fig)
-
-## --------------------------- search direction --------------------------- ##
-# include("search_direction.jl")
-
-
-# compute q
-# check if posdef R
-# newton method or gradient descent
-# get Ko,vo,Ro,Qo,So
-
-
-# for now:
-Ko,vo,q,z,v,y,Dh,D2g = PRONTO.search_direction(X1, U1, t, model, Kr, zeros(2));
-
-v = PRONTO.tau(τ->v(τ),t);
-
-fig = Figure()
-ax = Axis(fig[1,1]; title = "Ko"); plot_trajectory!(ax, t, Ko, 2)
-ax = Axis(fig[2,1]; title = "vo"); plot_trajectory!(ax, t, vo, 1)
-ax = Axis(fig[3,1]; title = "q"); plot_trajectory!(ax, t, q, 2)
-display(fig)
-
-
-## --------------------------- search direction --------------------------- ##
-
-fig = Figure()
-ax = Axis(fig[1,1]; title = "z"); plot_trajectory!(ax, t, z, 2)
-ax = Axis(fig[2,1]; title = "v"); plot_trajectory!(ax, t, v, 1)
-ax = Axis(fig[3,1]; title = "y"); plot_trajectory!(ax, t, y, 2)
-display(fig)
-
-# ζ = (z,v)
-
-## --------------------------- armijo --------------------------- ##
-
-γ = PRONTO.armijo_backstep(X1,U1,t,z,v,Kr,x0,f,l,p,Dh)
-
-## --------------------------- next estimate --------------------------- ##
-
-α = PRONTO.tau(t->(X1(t) + γ*z(t)), t);
-μ = PRONTO.tau(t->(U1(t) + γ*v(t)), t);
-X2,U2 = projection(α, μ, t, Kr, x0, f);
-fig = Figure()
-ax = Axis(fig[1,1]; title = "X1"); plot_trajectory!(ax, t, X1, 2)
-ax = Axis(fig[2,1]; title = "U1"); plot_trajectory!(ax, t, U1, 1)
-ax = Axis(fig[1,2]; title = "X2"); plot_trajectory!(ax, t, X2, 2)
-ax = Axis(fig[2,2]; title = "U2"); plot_trajectory!(ax, t, U2, 1)
-display(fig)
-
-
-
-
-
-
-
-
-
-## --------------------------- pronto loop --------------------------- ##
-
-X0 = X; U0 = U
-
-for iter in 1:200
-    @show iter
-    Kr,Pt = regulator(X0, U0, t, Rr, Qr, fx, fu);
-    X1,U1 = projection(X0, U0, t, Kr, x0, f);
-    Ko,vo,q,z,v,y,Dh,D2g = PRONTO.search_direction(X1, U1, t, model, Kr, zeros(2));
-
-    # end condition - always error
-    @show Dh
-    Dh > 0 ? (@error "increased cost from update direction"; break) : nothing
-    -Dh < 1e-2 ? (@error "converged - this is good"; break) : nothing
-
-    v = PRONTO.tau(τ->v(τ),t);
-    γ = PRONTO.armijo_backstep(X1,U1,t,z,v,Kr,x0,f,l,p,Dh)
-    α = PRONTO.tau(t->(X1(t) + γ*z(t)), t);
-    μ = PRONTO.tau(t->(U1(t) + γ*v(t)), t);
-    X0,U0 = projection(α, μ, t, Kr, x0, f);
-end
-
-fig = Figure()
-ax = Axis(fig[1,1]; title = "X (opt)"); plot_trajectory!(ax, t, X0, 2)
-ax = Axis(fig[2,1]; title = "U (opt)"); plot_trajectory!(ax, t, U0, 1)
-display(fig)
-
-
-
-
-
-
-
-## --------------------------- ?? --------------------------- ##
-
-
-fig = Figure(); ax = Axis(fig[1,1])
-lines!(ax, t, map(τ->X1(τ)[1], t))
-lines!(ax, t, map(τ->X1(τ)[2], t))
-display(fig)
-
-##
-
-swapdims(x) = permutedims(x, collect)
-
-Ro = R
-Ro += q*f
-
-
-##
-S₀ = t -> S(t) .+ sum(map(k->q(τ)[k]*Main.fuu(x(τ),u(τ))[k,:,:], 1:length(q(τ))))
-
-S₀ = τ -> S(τ) .+ sum(map( 1:length(q(τ)) ) do k
-    q(τ)[k]*fuu(x(τ), u(τ))[k,:,:]
-end)
-# end
+@time ξ = pronto(ξ, model)
 
 
 ## --------------------------- plot --------------------------- ##
-
-N = 2
-Y = Ko
-
-fig = Figure(); ax = Axis(fig[1,1])
-for i in 1:N
-    lines!(ax, t, map(τ->Y(τ)[i], t))
-end
-display(fig)
-
-## --------------------------- # --------------------------- ##
-
-
-## ---------------------------  --------------------------- ##
-
-
-# R₀ = R
-# Q₀ = Q
-# S₀ = S
-
-# R₀ = t -> R(t) .+ mapreduce((qk,fk)->qk*fk, sum, q(t), fuu(X1(t), U1(t)))
-
-# R₀ = t -> R(t) .+ sum(map((qk,fk) -> qk*fk, q(t), Main.fuu(X1(t), U1(t))))
-# Q₀ = t -> Q(t) .+ sum(map((qk,fk) -> qk*fk, q(t), Main.fxx(X1(t), U1(t))))
-# S₀ = t -> S(t) .+ sum(map((qk,fk) -> qk*fk, q(t), Main.fxu(X1(t), U1(t))))
-
-
-
-
-
-
-
-
-
-
+plot_timeseries(model.t, ξ[1], 2)
