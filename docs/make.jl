edeef65213748f031aa18d9a7d18650dd3f45c1b
mantas <mantas@case.edu>
2023-02-06 16:16:39 -0700

    docs site setup

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,24 @@
-using Pkg
-Pkg.activate(".")
-
 using Documenter
 # using PRONTO
 
 makedocs(
-    sitename="PRONTO.jl",
+    sitename = "PRONTO.jl",
     # modules = PRONTO
     authors = "Mantas Naris",
     pages = Any[
             "index.md"
         ],
-    doctest=false,
-    clean=true,
+    doctest = false,
+    clean = true,
 )
 
-#= ----------- uncomment for push to main & deploy ----------- #
 
 deploydocs(
-    repo = "github.com/lab-collab/lab-collab.github.io",
-    versions = nothing,
-    # devbranch = "main",
-    # devurl = ".",
+    repo = "github.com/narijauskas/PRONTO.jl.git",
+    # versions = ["stable" => "v^",],
+    versions = nothing, # temporary
+    devbranch = "main",
+    devurl = "dev",
+    push_preview = true,
 )
 
-# ----------------------------------------------------------- =#
