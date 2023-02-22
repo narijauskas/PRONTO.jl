9a71d46f50f39cb1a50b1bb1d46c61085fc7d2d7
jay <jish3262@colorado.edu>
2023-02-21 18:01:54 -0700

    correct 2 pi

--- test/test_3lvl.jl
+++ test/test_3lvl.jl
@@ -9,6 +9,7 @@ function mprod(x)
     return M   
 end
 
+
 function inprod(x)
     i = Int(length(x)/2)
     a = x[1:i]
@@ -58,10 +59,10 @@ function dynamics(x,u,t,θ)
     Ω2 = a2*u[1]
     Ω3 = a3*u[1]
     H1 = [0 Ω1 Ω3;Ω1 0 Ω2;Ω3 Ω2 0]
-    return mprod(-im*(H0+H1))*x
+    return 2*π*mprod(-im*(H0+H1))*x
 end
 
-# stagecost(x,u,t,θ) = 1/2 *θ[1]*collect(u')I*u
+# stagecost(x,u,t,θ) = 1/2 * (θ[1]*collect(u')I*u + 1*collect(x')*mprod(diagm([0, 0, 1]))*x)
 
 function kll(t)
     return 0.01+10*(exp(-2*t)+exp(2*t-30))
@@ -99,20 +100,16 @@ function plot_3lvl(ξ,τ)
     fig = Figure()
     ts = LinRange(τ...,1001)
 
-    ax = Axis(fig[1:2,1]; title="state")
-    is = eachindex(ξ.x)
-    xs = [ξ.x(t)[i] for t∈ts, i∈is]
-    foreach(i->lines!(ax, ts, xs[:,i]), is)
-    
-    ax = Axis(fig[1:2,2]; title="population")
-    ps = ([I(3) I(3)] * (xs.^2)')'
-    foreach(i->lines!(ax, ts, ps[:,i]), 1:3)
-
-
-    ax = Axis(fig[3,1:2]; title="inputs")
+    ax = Axis(fig[1:2,1]; title="input")
     is = eachindex(ξ.u)
     us = [ξ.u(t)[i] for t∈ts, i∈is]
     foreach(i->lines!(ax, ts, us[:,i]), is)
+    
+    ax = Axis(fig[1:2,2]; title="population")
+    is = eachindex(ξ.x)
+    xs = [ξ.x(t)[i] for t∈ts, i∈is]
+    ps = ([I(3) I(3)] * (xs.^2)')'
+    foreach(i->lines!(ax, ts, ps[:,i]), 1:3)
 
     return fig
 end
@@ -125,7 +122,8 @@ t0,tf = τ = (0,15)
 
 
 θ = lvl3(kl=0.01, kr=1, kq=1)
-μ = @closure t->SVector{1}(0.5*sin(10*t))
+# μ = @closure t->SVector{1}(0.5*sin(10*t))
+μ = @closure t->SVector{1}((π/tf)*exp(-(t-tf/2)^2/(tf^2))*cos(2*π*1*t))
 φ = open_loop(θ,x0,μ,τ)
 @time ξ = pronto(θ,x0,φ,τ; tol = 1e-4, maxiters = 50, limitγ = true)
 
