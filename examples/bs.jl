1514a2c4699eec4404fa49404fdb57356bcf804c
jay <jish3262@colorado.edu>
2023-06-29 16:35:57 -0600

    bs update

--- examples/bs.jl
+++ examples/bs.jl
@@ -38,14 +38,14 @@ end
 
 ## ------------------------------- beam splitter to eigenstate 2 ------------------------------- ##
 
-@kwdef struct bs2 <: PRONTO.Model{NX,NU}
+@kwdef struct Bs2 <: PRONTO.Model{NX,NU}
     kl::Float64 # stage cost gain
     kr::Float64 # regulator r gain
     kq::Float64 # regulator q gain
 end
 
 
-@dynamics bs2 begin
+@dynamics Bs2 begin
     α = 10 
     v = -α/4
     H0 = SymTridiagonal(promote([4.0i^2 for i in -N:N], v*ones(2N))...)
@@ -54,32 +54,32 @@ end
     return mprod(-im*(H0 + sin(u[1])*H1 + (1-cos(u[1]))*H2) )*x
 end
 
-@stage_cost bs2 begin
+@stage_cost Bs2 begin
     θ.kl/2*u'*I*u 
 end
 
-@terminal_cost bs2 begin
+@terminal_cost Bs2 begin
     P = I(NX) - inprod(x_eig(2))
     return 1/2*x'*P*x
 end
 
-@regulatorQ bs2 begin
+@regulatorQ Bs2 begin
     x_re = x[1:2N+1]
     x_im = x[2N+2:NX]
     ψ = x_re + im*x_im
     return θ.kq*mprod(I(2N+1) - ψ*ψ')
 end
 
-@regulatorR bs2 θ.kr*I(NU)
+@regulatorR Bs2 θ.kr*I(NU)
 
 # must be run after any changes to model definition
-resolve_model(bs2)
+resolve_model(Bs2)
 
 # overwrite default behavior of Pf
-PRONTO.Pf(θ::bs2,α,μ,tf) = SMatrix{NX,NX,Float64}(I(NX)-α*α')
+PRONTO.Pf(θ::Bs2,α,μ,tf) = SMatrix{NX,NX,Float64}(I(NX)-α*α')
 
 # runtime plots
-PRONTO.runtime_info(θ::bs2, ξ; verbosity=1) = verbosity >= 1 && println(preview(ξ.u, 1))
+PRONTO.runtime_info(θ::Bs2, ξ; verbosity=1) = verbosity >= 1 && println(preview(ξ.u, 1))
 
 
 ## ------------------------------- demo: eigenstate 1->2 in 10s ------------------------------- ##
@@ -90,7 +90,7 @@ xf = SVector{NX}(x_eig(2))
 t0,tf = τ = (0,10)
 
 
-θ = bs2(kl=0.01, kr=1, kq=1)
+θ = Bs2(kl=0.01, kr=1, kq=1)
 μ = t->SVector{NU}(0.4*sin(t))
 φ = open_loop(θ,x0,μ,τ)
 @time ξ = pronto(θ,x0,φ,τ; tol = 1e-6, maxiters = 50, limitγ = true, verbosity=2)
@@ -105,63 +105,69 @@ file = matopen("Uopt_nu2_10_1.0.mat","w")
 write(file,"Uopt",us)
 close(file)
 
+## ------------------------------- beam splitter to eigenstate 2 ------------------------------- ##
+
+@kwdef struct Bs4 <: PRONTO.Model{NX,NU}
+    kl::Float64 # stage cost gain
+    kr::Float64 # regulator r gain
+    kq::Float64 # regulator q gain
+end
+
+
+@dynamics Bs4 begin
+    α = 10 
+    v = -α/4
+    H0 = SymTridiagonal(promote([4.0i^2 for i in -N:N], v*ones(2N))...)
+    H1 = v*im*Tridiagonal(ones(2N), zeros(2N+1), -ones(2N))
+    H2 = v*Tridiagonal(-ones(2N), zeros(2N+1), -ones(2N))
+    return mprod(-im*(H0 + sin(u[1])*H1 + (1-cos(u[1]))*H2) )*x
+end
+
+@stage_cost Bs4 begin
+    θ.kl/2*u'*I*u 
+end
+
+@terminal_cost Bs4 begin
+    P = I(NX) - inprod(x_eig(4))
+    return 1/2*x'*P*x
+end
+
+@regulatorQ Bs4 begin
+    x_re = x[1:2N+1]
+    x_im = x[2N+2:NX]
+    ψ = x_re + im*x_im
+    return θ.kq*mprod(I(2N+1) - ψ*ψ')
+end
+
+@regulatorR Bs4 θ.kr*I(NU)
+
+# must be run after any changes to model definition
+resolve_model(Bs4)
+
+# overwrite default behavior of Pf
+PRONTO.Pf(θ::Bs4,α,μ,tf) = SMatrix{NX,NX,Float64}(I(NX)-α*α')
+
+# runtime plots
+PRONTO.runtime_info(θ::Bs4, ξ; verbosity=1) = verbosity >= 1 && println(preview(ξ.u, 1))
+
+
 ## ------------------------------- demo: eigenstate 1->4 in 10s ------------------------------- ##
 
-x0 = SVector{22}(x_eig(1))
-xf = SVector{22}(x_eig(4))
-t0,tf = τ = (0,10)
+x0 = SVector{NX}(x_eig(1))
+xf = SVector{NX}(x_eig(4))
+t0,tf = τ = (0,1.5)
 
 
-θ = Split4(kl=0.01, kr=1, kq=1)
-μ = @closure t->SVector{1}(0.5*sin(t))
+θ = Bs4(kl=0.01, kr=1, kq=1)
+μ = t->SVector{1}(0.5*sin(t))
 φ = open_loop(θ,x0,μ,τ)
-@time ξ = pronto(θ,x0,φ,τ; tol = 1e-4, maxiters = 50, limitγ = true)
-
-# plot_split(ξ,τ)
+@time ξ = pronto(θ,x0,φ,τ; tol = 1e-4, maxiters = 50, limitγ = true, verbosity=1)
 
 
-## ------------------------------- demo: eigenstate 1->4 in 2s ------------------------------- ##
-
-x0 = SVector{22}(x_eig(1))
-xf = SVector{22}(x_eig(4))
-t0,tf = τ = (0,2.55)
-
-
-θ = Split4(kl=0.02, kr=1, kq=1)
-μ = @closure t->SVector{1}(0.5*sin(t))
-φ = open_loop(θ,x0,μ,τ)
-ξ = pronto(θ,x0,φ,τ; tol = 1e-6, maxiters = 100, limitγ = true)
-
-plot_split(ξ,τ)
-
-
-
-
-
-
-
-
-## ------------------------------- step-by-step debugging ------------------------------- ##
-
-Kr = PRONTO.regulator(θ,φ,τ)
-ξ = PRONTO.projection(θ,x0,φ,Kr,τ)
-
-λ = PRONTO.lagrangian(θ,ξ,φ,Kr,τ)
-Ko = PRONTO.optimizer(θ,λ,ξ,φ,τ)
-vo = PRONTO.costate(θ,λ,ξ,φ,Ko,τ)
-
-
-ζ = PRONTO.search_direction(θ,ξ,Ko,vo,τ)
-γ = 0.7
-ξ1 = PRONTO.armijo_projection(θ,x0,ξ,ζ,γ,Kr,τ)
-
-
-## ------------------------------- other ------------------------------- ##
-
-
-# shorter error messages:
-using SciMLBase, DifferentialEquations
-Base.show(io::IO, ::Type{<:SciMLBase.ODEProblem}) = print(io, "ODEProblem{...}")
-Base.show(io::IO, ::Type{<:OrdinaryDiffEq.ODEIntegrator}) = print(io, "ODEIntegrator{...}")
-
-
+##
+ts = t0:0.001:tf
+is = eachindex(ξ.u)
+us = [ξ.u(t)[i] for t∈ts, i∈is]
+file = matopen("Uopt_nu4_1.5T_1.0.mat","w")
+write(file,"Uopt",us)
+close(file)
