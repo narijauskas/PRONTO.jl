40664a620ebef526a38adf0a678b0b9bbb2b0ebd
jay <jish3262@colorado.edu>
2023-09-08 14:52:25 -0600

    bs_1.5T_result

--- examples/split.jl
+++ examples/split.jl
@@ -35,13 +35,13 @@ end
 
 ## ----------------------------------- define the model ----------------------------------- ##
 
-@kwdef struct Split2 <: PRONTO.Model{22,1}
+@kwdef struct Split4 <: PRONTO.Model{22,1}
     kl::Float64 # stage cost gain
     kr::Float64 # regulator r gain
     kq::Float64 # regulator q gain
 end
 
-@define_f Split2 begin
+@define_f Split4 begin
     α = 10 
     v = -α/4
     N = 5
@@ -51,37 +51,37 @@ end
     return mprod(-im*(H0 + sin(u[1])*H1 + (1-cos(u[1]))*H2) )*x
 end
 
-@define_l Split2 begin
+@define_l Split4 begin
     kl/2*u'*I*u 
 end
 
-@define_m Split2 begin
-    P = I(22) - inprod(x_eig(2))
+@define_m Split4 begin
+    P = I(22) - inprod(x_eig(4))
     return 1/2*x'*P*x
 end
 
-@define_Q Split2 begin
+@define_Q Split4 begin
     x_re = x[1:11]
     x_im = x[12:22]
     ψ = x_re + im*x_im
     return kq*mprod(I(11) - ψ*ψ')
 end
 
-@define_R Split2 kr*I(1)
+@define_R Split4 kr*I(1)
 
 # must be run after any changes to model definition
-resolve_model(Split2)
+resolve_model(Split4)
 
-PRONTO.Pf(θ::Split2,α,μ,tf) = SMatrix{22,22,Float64}(I(22)-α*α')
-PRONTO.γmax(θ::Split2, ζ, τ) = PRONTO.sphere(1, ζ, τ)
-PRONTO.preview(θ::Split2, ξ) = [I(11) I(11)]*(ξ.x.^2)
+PRONTO.Pf(θ::Split4,α,μ,tf) = SMatrix{22,22,Float64}(I(22)-inprod(α))
+PRONTO.γmax(θ::Split4, ζ, τ) = PRONTO.sphere(1, ζ, τ)
+PRONTO.preview(θ::Split4, ξ) = [I(11) I(11)]*(ξ.x.^2)
 
 ## ----------------------------------- solve the problem ----------------------------------- ##
-# eigenstate 1->2
+# eigenstate 1->4
 
-θ = Split2(kl=0.01, kr=1, kq=1)
-t0,tf = τ = (0,10)
+θ = Split4(kl=0.01, kr=1, kq=1)
+t0,tf = τ = (0,1.5)
 x0 = SVector{22}(x_eig(1))
 μ = t->SVector{1}(0.4*sin(t))
 η = open_loop(θ,x0,μ,τ)
-ξ,data = pronto(θ,x0,η,τ);
+ξ,data = pronto(θ,x0,η,τ;tol=1e-4,maxiters=50);
