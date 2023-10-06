bb12b8345b20737d0fd71c6b4e496619feb9758a
jay <jish3262@colorado.edu>
2023-10-05 20:12:55 -0600

    increased N to 4

--- examples/split_hold.jl
+++ examples/split_hold.jl
@@ -16,7 +16,7 @@ end
 function x_eig(i)
     α = 10 
     v = -α/4
-    N = 3
+    N = 4
     H0 = SymTridiagonal(promote([4.0i^2 for i in -N:N], v*ones(2N))...)
     w = eigvecs(collect(H0)) # symbolic doesn't work here
     x_eig = kron([1;0],w[:,i])
@@ -24,13 +24,13 @@ end
 
 ## ----------------------------------- define the model ----------------------------------- ##
 
-@kwdef struct SplitHold <: Model{28,1}
+@kwdef struct SplitHold <: Model{36,1}
     kl::Float64 # stage cost gain
 end
 
 @define_f SplitHold begin
     ω = 1.0
-    n = 3
+    n = 4
     α = 10
     v = -α/4
     H0 = SymTridiagonal(promote([4.0i^2 for i in -n:n], v*ones(2n))...)
@@ -47,35 +47,35 @@ end
 end
 
 @define_m SplitHold begin
-    ψ0 = x_eig(1)[1:7]
-    ψ1 = x_eig(2)[1:7]
+    ψ0 = x_eig(1)[1:9]
+    ψ1 = x_eig(2)[1:9]
     xf = vec([ψ0;ψ1;0*ψ0;0*ψ1])
-    P = I(28)
+    P = I(36)
     return 1/2 * (x-xf)'*P*(x-xf)
 end
 
 
-@define_Q SplitHold I(28)
+@define_Q SplitHold I(36)
 @define_R SplitHold I(1)
 
 resolve_model(SplitHold)
 
 
 PRONTO.preview(θ::SplitHold, ξ) = ξ.u
-PRONTO.Pf(θ::SplitHold, αf, μf, tf) = SMatrix{28,28,Float64}(I(28))
+PRONTO.Pf(θ::SplitHold, αf, μf, tf) = SMatrix{36,36,Float64}(I(36))
 
 ## ----------------------------------- solve the problem ----------------------------------- ##
 
-ψ3 = x_eig(4)[1:7]
-ψ4 = x_eig(5)[1:7]
-x0 = SVector{28}(vec([ψ3;ψ4;0*ψ3;0*ψ4]))
+ψ3 = x_eig(4)[1:9]
+ψ4 = x_eig(5)[1:9]
+x0 = SVector{36}(vec([ψ3;ψ4;0*ψ3;0*ψ4]))
 t0,tf = τ = (0,2.8)
 
 
 θ = SplitHold(kl=0.01)
 μ = t->SVector{1}(1.0*sin(4*t))
 η = open_loop(θ,x0,μ,τ)
-ξ,data = pronto(θ,x0,η,τ;tol=1e-3,maxiters=50);
+@time ξ,data = pronto(θ,x0,η,τ;tol=1e-3,maxiters=50);
 
 ##
 import Pkg
@@ -85,6 +85,6 @@ using MAT
 
 ts = t0:0.001:tf
 us = [ξ.u(t)[1] for t in ts]
-file = matopen("split_hold_2.8T_3N.mat", "w")
+file = matopen("split_hold_2.8T_4N.mat", "w")
 write(file, "Uopt", us)
 close(file)
