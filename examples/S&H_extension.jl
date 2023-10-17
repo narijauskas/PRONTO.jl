3ebd48c3a1ebd3bc64982ffe1b0067e8a713cd59
jay <jish3262@colorado.edu>
2023-10-17 11:13:26 -0600

    change initial guess frequency

--- examples/S&H_extension.jl
+++ examples/S&H_extension.jl
@@ -16,7 +16,7 @@ end
 function x_eig(i)
     α = 10 
     v = -α/4
-    N = 3
+    N = 4
     H0 = SymTridiagonal(promote([4.0i^2 for i in -N:N], v*ones(2N))...)
     w = eigvecs(collect(H0)) # symbolic doesn't work here
     x_eig = kron([1;0],w[:,i])
@@ -24,13 +24,13 @@ end
 
 ## ----------------------------------- define the model ----------------------------------- ##
 
-@kwdef struct SplitHold <: Model{29,1}
+@kwdef struct SplitHold <: Model{37,1}
     kl::Float64 # stage cost gain
 end
 
 @define_f SplitHold begin
     ω = 1.0
-    n = 3
+    n = 4
     α = 10
     v = -α/4
     H0 = SymTridiagonal(promote([4.0i^2 for i in -n:n], v*ones(2n))...)
@@ -39,7 +39,7 @@ end
     H11 = kron(I(2),H1)   
     H2 = v*Tridiagonal(-ones(2n), zeros(2n+1), -ones(2n))
     H22 = kron(I(2),H2)
-    return [mprod(-im*ω*(H00 + sin(x[29])*H11 + (1-cos(x[29]))*H22) )*x[1:28];u[1]]
+    return [mprod(-im*ω*(H00 + sin(x[37])*H11 + (1-cos(x[37]))*H22) )*x[1:36];u[1]]
 end
 
 @define_l SplitHold begin
@@ -47,35 +47,35 @@ end
 end
 
 @define_m SplitHold begin
-    ψ0 = x_eig(1)[1:7]
-    ψ1 = x_eig(2)[1:7]
+    ψ0 = x_eig(1)[1:9]
+    ψ1 = x_eig(2)[1:9]
     xf = vec([ψ0;ψ1;0*ψ0;0*ψ1])
-    P = I(28)
-    return 1/2 * (x[1:28]-xf)'*P*(x[1:28]-xf)
+    P = I(36)
+    return 1/2 * (x[1:36]-xf)'*P*(x[1:36]-xf)
 end
 
 
-@define_Q SplitHold I(29)
+@define_Q SplitHold I(37)
 @define_R SplitHold I(1)
 
 resolve_model(SplitHold)
 
 
 PRONTO.preview(θ::SplitHold, ξ) = ξ.u
-PRONTO.Pf(θ::SplitHold, αf, μf, tf) = SMatrix{29,29,Float64}(I(29))
+PRONTO.Pf(θ::SplitHold, αf, μf, tf) = SMatrix{37,37,Float64}(I(37))
 
 ## ----------------------------------- solve the problem ----------------------------------- ##
 
-ψ3 = x_eig(4)[1:7]
-ψ4 = x_eig(5)[1:7]
-x0 = SVector{29}(vec([ψ3;ψ4;0*ψ3;0*ψ4;0]))
+ψ3 = x_eig(4)[1:9]
+ψ4 = x_eig(5)[1:9]
+x0 = SVector{37}(vec([ψ3;ψ4;0*ψ3;0*ψ4;0]))
 t0,tf = τ = (0,2.8)
 
 
 θ = SplitHold(kl=0.001)
-μ = t->SVector{1}(1.0*sin(4*t))
+μ = t->SVector{1}(1.0*sin(12*t))
 η = open_loop(θ,x0,μ,τ)
-ξ,data = pronto(θ,x0,η,τ;tol=1e-6,maxiters=50);
+ξ,data = pronto(θ,x0,η,τ;tol=1e-4,maxiters=50);
 
 ##
 import Pkg
@@ -85,6 +85,6 @@ using MAT
 
 ts = t0:0.001:tf
 us = [ξ.x(t)[end] for t in ts]
-file = matopen("split_hold_2.8T_3N_EX.mat", "w")
+file = matopen("split_hold_2.8T_4N_EX_12w.mat", "w")
 write(file, "Uopt", us)
 close(file)
